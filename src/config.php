7bec299072a7c292f957d10710d18809c4ce1e26
aalfiann@gmail.com <aalfiann@gmail.com>
2016-12-16 02:15:49 +0700

    update path config

--- src/config.php
+++ src/config.php
@@ -5,9 +5,9 @@ use lib\Config;
 // DB Config
 Config::write('db.host', 'localhost');
 Config::write('db.port', '3306');
-Config::write('db.basename', 'javelinee');
+Config::write('db.basename', 'islim');
 Config::write('db.user', 'root');
 Config::write('db.password', 'root');
 
 // Project Config
-Config::write('path', 'http://localhost:1337/islim');
+Config::write('path', 'http://localhost:1337/islim/src/');
